89bf82a718b2cce77d4f05b9a03e96840213bd34
Jonathan Humphreys <jphcode@gmail.com>
2021-10-18 19:17:44 +0100

    added a basic server status icon

--- src/components/serverStatus.jsx
+++ src/components/serverStatus.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import './serverStatus.css';
+import Ping from '../api/CoinGecko';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPowerOff } from '@fortawesome/free-solid-svg-icons'
 
 class ServerStatus extends React.Component {
 
@@ -8,17 +12,38 @@ class ServerStatus extends React.Component {
         status: false
     }
 
+    async componentDidMount() {
+        const status = await Ping();
+
+        this.setState({
+            status: status
+        });
+    }
+
     render() {
+
+        let iconStyle;
+
+        if (this.state.status) {
+            iconStyle = {
+                color: "green",
+            }
+        }
+        else {
+            iconStyle = {
+                color: "red",
+            }
+        }
+
+
+
         return (
             <div className="server-status-container">
-                <div className="server-status-name">
-
+                <div className="server-status server-status-name">
+                    <p>{this.props.name}</p>
                 </div>
-                <div className="server-status-icon">
-
-                </div>
-                <div className="server-status-status">
-
+                <div className="server-status server-status-status">
+                    <FontAwesomeIcon icon={faPowerOff} style={iconStyle} size="lg" />
                 </div>
             </div>
         )
--- src/pages/validators.jsx
+++ src/pages/validators.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Validator from '../components/validator';
+import ServerStatus from '../components/serverStatus';
+import CoinGecko from '../crypto/coingecko.png';
 
 class Validators extends React.Component {
     render() {
         return (
             <div>
+                <ServerStatus
+                    name="CoinGecko"
+                    icon={CoinGecko}
+                />
                 <Validator />
             </div>
         )
