e1dbfde327395c07926fa679360b181f6f566730
Jonathan Humphreys <jph.code@pm.me>
2021-11-13 15:15:10 +0000

    centred the navbar and added an underline

--- src/components/navbarComponent.jsx
+++ src/components/navbarComponent.jsx
@@ -6,13 +6,8 @@ class NavbarComponent extends React.Component {
     render() {
         return (
             <Navbar collapseOnSelect expand="lg" id="navbar" bg="dark" variant="dark">
-                <Navbar.Brand href="/">UK Pools</Navbar.Brand>
+                <Navbar.Brand href="/">UK POOLS</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="ml-auto">
-                        <Nav.Link href="/validators" id="validator-nav-button" className="navbar-button">Validators</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
             </Navbar>
         )
     }
